136f6d816ac13b387e261d22fb805086f165ad2b
Tarciana Oliveira <anarumiroan@gmail.com>
2023-08-28 20:14:20 -0300

    atualizações

--- index.js
+++ index.js
@@ -17,12 +17,25 @@ const operacaoPendente = () => operador !== 'undefined';
 const calcular = () => {
 
     if (operacaoPendente()) {
-        const numeroAtual = parseFloat(display.textContent);
-        if (operador === '+') {
-            atualizarDisplay(numeroAnterior + numeroAtual)
+        if (operacaoPendente()) {
+            const numeroAtual = parseFloat(display.textContent);
+
+            novoNumero = true;
+
+            if (operador === '+') {
+                atualizarDisplay(numeroAnterior + numeroAtual)
+            } else if (operador === '-') {
+                atualizarDisplay(numeroAnterior - numeroAtual)
+            } else if (operador === '*') {
+                atualizarDisplay(numeroAnterior * numeroAtual)
+            } else if (operador === '/') {
+                atualizarDisplay(numeroAnterior / numeroAtual)
+            }
         }
+
     }
 }
+
 
 
 const atualizarDisplay = (texto) => {
@@ -30,7 +43,7 @@ const atualizarDisplay = (texto) => {
         display.textContent = texto;
         novoNumero = false;
     } else {
-        display.textContent = texto;
+        display.textContent += texto;
     }
 }
 
@@ -49,3 +62,45 @@ const selecionarOperador = (evento) => {
 }
 
 operadores.forEach(opera => opera.addEventListener('click', selecionarOperador));
+
+
+const ativarIgual = () => {
+    calcular();
+    operador = undefined;
+}
+document.getElementById('igual').addEventListener('click', ativarIgual);
+
+const limparDisplay = () => display.textContent = '';
+
+document.getElementById('limparDisplay').addEventListener('click', limparDisplay);
+
+
+const limparCalculo = () => {
+    limparDisplay();
+    operador = undefined;
+    novoNumero = true;
+    numeroAnterior = undefined;
+}
+
+
+document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+
+
+
+const removerUltimo = () => {
+    display.textContent = display.textContent.slice(0, -1);
+}
+document.getElementById('backspace').addEventListener('click', removerUltimo);
+
+
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+}
+
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
+
+
+
+document.getElementById('decimal').addEventListener('click', inserirDecimal);
